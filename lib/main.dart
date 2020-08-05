92da0f0df18ddfb88b5eb3adcfae7ac630fedd67
Abinash S <s.abinash@outlook.com>
2020-08-05 09:15:18 +0530

    Project building - basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/choose_location.dart';
+import 'package:worldtime/pages/loading.dart';
 
 void main() =>
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location' : (context) => ChooseLocation(),
+    },
   ));
 
 
